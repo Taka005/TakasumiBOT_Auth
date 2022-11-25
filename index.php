111b65c534ff415e5b25e131deec7c02a38ac8ff
Taka005 <01taka005@gmail.com>
2022-11-25 12:26:51 +0900

    テスト

--- index.php
+++ index.php
@@ -16,8 +16,10 @@ function is_animated($image){
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>TakasumiBOT Members</title>
-
+<!--
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css" integrity="sha384-DhY6onE6f3zzKbjUPRc2hOzGAdEf4/Dz+WJwBvEYL/lkkIsI3ihufq9hk9K4lVoK" crossorigin="anonymous">
+-->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/style.css">
 
     <link rel="apple-touch-icon" type="image/png" sizes="180x180" href="./images/apple-touch-icon-180x180.png">
