2c23ebb7e5d46457e62f0d6a4484027bd39ed4bf
Taka005 <01taka005@gmail.com>
2022-11-25 13:04:50 +0900

    修正

--- index.php
+++ index.php
@@ -16,9 +16,7 @@ function is_animated($image){
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>TakasumiBOT Members</title>
-<!--
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css" integrity="sha384-DhY6onE6f3zzKbjUPRc2hOzGAdEf4/Dz+WJwBvEYL/lkkIsI3ihufq9hk9K4lVoK" crossorigin="anonymous">
--->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/style.css">
 
