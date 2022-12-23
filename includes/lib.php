59ee5d81968c37f920928780fdfdb33e1b96deb1
Taka005 <01taka005@gmail.com>
2022-12-23 18:41:46 +0900

    ミス

--- includes/lib.php
+++ includes/lib.php
@@ -1,6 +1,8 @@
+<?php
 function sql($query){
     $database = require __DIR__."/../database.php";
     $pdo = new PDO("mysql:host=".$database["server"].";dbname=".$database["name"].";charset=utf8",$database["user"],$database["password"]);
     $res = $pdo->query($query);
     return $res;
 }
+?>
