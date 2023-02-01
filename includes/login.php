98625e18c60d4514aaf9f3c61e9dc2312d61c26e
Taka005 <01taka005@gmail.com>
2023-02-01 20:56:17 +0900

    修正

--- includes/login.php
+++ includes/login.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . "/discord.php";
 require_once "../config.php";
 
-init($redirect_url, $client_id, $secret_id);
+init($redirect_url,$client_id,$secret_id);
 get_user();
 
 header("Location: ../");
