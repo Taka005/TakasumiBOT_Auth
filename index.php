ecd9965f42e278da03fe3752d65278704915724a
Taka005 <01taka005@gmail.com>
2023-02-05 10:45:17 +0900

    修正

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ function is_animated($image){
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>TakasumiBOT Members</title>
+    <title>TakasumiBOT Auth</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css" integrity="sha384-DhY6onE6f3zzKbjUPRc2hOzGAdEf4/Dz+WJwBvEYL/lkkIsI3ihufq9hk9K4lVoK" crossorigin="anonymous">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/style.css">
@@ -31,18 +31,18 @@ function is_animated($image){
     <meta property="og:url" content="https://auth.taka.ml/" />
     <meta property="og:type" content="website" />
     <meta property="og:title" content="トップページ" />
-    <meta property="og:description" content="TakasumiBOT Members登録サイト。DiscordアカウントとBOTを紐付けることによりさまざまなサービスが簡単に使えるようになります" />
-    <meta property="og:site_name" content="TakasumiBOT Members" />
+    <meta property="og:description" content="TakasumiBOT Web認証システム" />
+    <meta property="og:site_name" content="TakasumiBOT Auth" />
     <meta property="og:image" content="https://auth.taka.ml/images/takasumibot.png" />
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="TakasumiBOT Members登録サイト。DiscordアカウントとBOTを紐付けることによりさまざまなサービスが簡単に使えるようになります">
-    <meta name="keywords" content="Discord,TakasumiBOT Members,TakasumiBOT,Taka005">
+    <meta name="description" content="TakasumiBOT Web認証システム">
+    <meta name="keywords" content="Discord,TakasumiBOT Auth,TakasumiBOT,Taka005">
 
 </head>
 <body>
 	<main class="p-0 base">
-		<h1 class="text-center text-light">TakasumiBOT Members</h1>
+		<h1 class="text-center text-light">TakasumiBOT Auth</h1>
 	<?php
 	if(isset($_SESSION["user"])){
 		$avatar_url = "https://cdn.discordapp.com/avatars/".$_SESSION["user_id"]."/".$_SESSION["user_avatar"].is_animated($_SESSION["user_avatar"]);
