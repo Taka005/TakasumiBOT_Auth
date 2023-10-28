18a9709e4f618c3dd7876f98fb82b1bd4f3ca98d
Taka005 <01taka005@gmail.com>
2023-10-28 12:12:37 +0900

    change

--- includes/login.php
+++ includes/login.php
@@ -4,7 +4,8 @@ require_once __DIR__."/lib.php";
 require_once "../config.php";
 
 $mute = DB::query("SELECT * FROM mute_ip WHERE ip = '".$_SERVER["REMOTE_ADDR"]."';")->fetchALL();
-if($mute[0]) return print("アクセスが許可されていません");
+if($mute[0]) return http_response_code(401)
+//print("アクセスが許可されていません");
 
 Oauth($redirect_url,$client_id,$secret_id);
 getUser();
