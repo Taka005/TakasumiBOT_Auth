6d3bb24e07ab759dccff85b0c4470561d11abbcb
Taka005 <takanosumika.sub@gmail.com>
2023-10-28 10:01:28 +0900

    style

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-require_once __DIR__ . "/includes/discord.php";
-require_once __DIR__ . "/config.php";
+require_once __DIR__."/includes/discord.php";
+require_once __DIR__."/config.php";
 ?>
 <html lang="ja">
 	<head>
